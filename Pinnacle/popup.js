93d6f2c9842f465b61e2af1a075eb5c9b7f7bc2e
cnelson20 <cjnelson2004@gmail.com>
2022-03-27 04:03:47 +0100

    code

--- Pinnacle/popup.js
+++ Pinnacle/popup.js
@@ -3,35 +3,12 @@ const viewButton = document.getElementById('viewCommentsButton');
 const commentText = document.getElementById('commentText');
 
 const tabPromise = chrome.tabs.query({ active: true, currentWindow: true });
-const customCSS = ".pinnacle-comment-wrapper {\
-    position: relative;\
-}\
-.pinnacle-comment {\
-    padding : 2px;\
-    position: absolute;\
-    background-color: rgba(173, 216, 230, 0.5);\
-    align-self: right;\
-    text-align: center;\
-    min-width: 200px;\
-    right: 30;\
-    bottom: 0;\
-}\
-.pinnacle-text-formatter {\
-	color : black;\
-	text-decoration:none;\
-	font-size:1rem;\
-	font-family:Calibri;\
-}\
-\
-.pinnacle-anchor-highlight {\
-    background-color: rgba(150, 150, 150, 0.5);\
-}";
 
 async function insertCustomCSS() {
     let [tab] = await tabPromise;
     chrome.scripting.insertCSS({
         target : {tabId : tab.id},
-        css : customCSS,
+        files : ['pinnacle.css'],
     });
 }
 
